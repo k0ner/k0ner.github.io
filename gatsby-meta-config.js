64940cacef4833276301fb34f6229557534d52d7
Konrad Tendera <k0ner@users.noreply.github.com>
2022-12-12 15:55:15 -0800

    Cleans up for a new blog (#2)

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -17,14 +17,14 @@
 
 /** @type {MetaConfig} */
 const metaConfig = {
-  title: "Dev Ed",
-  description: `Ed's Blog`,
-  author: "Ed",
-  siteUrl: "https://gatsby-starter-apple.netlify.app",
+  title: "DevStories",
+  description: `Dev stories`,
+  author: "Konrad",
+  siteUrl: "https://devstories.pl",
   lang: "en",
-  utterances: "sungik-choi/gatsby-starter-apple-comment",
+  utterances: "k0ner/devstories-comment",
   links: {
-    github: "https://github.com/sungik-choi/gatsby-starter-apple",
+    github: "https://github.com/k0ner",
   },
   favicon: "src/images/icon.png",
 }
--- src/posts/about.md
+++ src/posts/about.md
@@ -1,6 +1,4 @@
 # Hello
 
-Welcome to my blog. My name is Ed.<br>
-I'm very interested in the web front-end development.
-
-Visit my [Github repository](https://github.com/sungik-choi/gatsby-starter-apple).
+Welcome to my blog. My name is Konrad.<br>
+I'm very interested in the web software development
